3e40f965d43fb0ff3f819f1906d055ec7258df47
Viktor <qbodie0@gmail.com>
2025-05-06 19:27:35 +0300

    Realisation of loggining

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -4,6 +4,10 @@ from app.schemas.user import UserCreate
 from app.models.user import User
 from app.core.security import get_password_hash
 from app.db.session import get_db
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from app.core.security import verify_password, create_access_token
+from app.core.config import settings
 
 router = APIRouter()
 
@@ -21,4 +25,15 @@ def register(user_in: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
     return {"email": user.email, "id": user.id}
+
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
+
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
